ea49884995c48e6a672d989ed9199654f85eff81
AdanGarza <158204326+AdanGarza@users.noreply.github.com>
2026-06-11 22:24:32 +0000

    pesudocode for the program

---
+++ assignment4.cpp
@@ -0,0 +1,40 @@
+tart
+
+Ask the user for:
+- Nightly room rate
+- Number of rooms
+- Number of nights
+- Sales tax rate
+
+Check that the values are valid.
+If any value is invalid, display an error.
+
+Set discount to 0%.
+
+If rooms are 30 or more
+    discount = 30%
+Else if rooms are 20 or more
+    discount = 20%
+Else if rooms are 10 or more
+    discount = 10%
+
+Calculate subtotal:
+subtotal = nightly rate × rooms × nights
+
+Apply the room discount.
+
+If nights are 3 or more
+    Apply an extra 5% loyalty discount.
+
+Calculate sales tax.
+Calculate final total.
+
+Display which discount was applied:
+- No discount
+- 10% discount
+- 20% discount
+- 30% discount
+
+Print the final receipt.
+
+End
